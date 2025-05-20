a9f260a503cbcbff096634b5ecdbf3b869254485
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-20 18:59:26 +0200

    aadd: receiving and sending messages functions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ choice = input("Do you want to host[1] or to connect[2]?: ")
 
 if choice == "1":
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("192.168.1.38", 9999))
+    server.bind(("192.168.1.1", 9999))
     server.listen()
 
     print("Server is listening...")
@@ -14,7 +14,21 @@ if choice == "1":
     client, _ = server.accept()
 elif choice == "2":
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("192.168.1.38", 9999))
+    client.connect(("192.168.1.1", 9999))
     print("Connected successfully!")
 else:
     print("Invalid choice, retry")
+
+
+def sending_messages(c):
+    while True:
+        message = input("")
+        c.send(message.encode())
+        print("You: " + message)
+
+def receiving_messages(c):
+    while True:
+        print("Partner: " + c.recv(1024).decode())
+
+threading.Thread(target=sending_messages, args=(client,)).start
+threading.Thread(target=receiving_messages, args=(client,)).start
