68a1d3d3c86a2e39683501fb72a18f42d3308ed6
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-20 18:33:53 +0200

    add: second choice to connect

--- main.py
+++ main.py
@@ -9,4 +9,12 @@ if choice == "1":
     server.bind(("IP ADDRESS", 9999))
     server.listen()
 
+    print("Server is listening...")
+
     client, _ = server.accept()
+elif choice == "2":
+    client = socket.socket(socket.AFI_INET, socket.SOCK_STREAM)
+    client.connect(("IP ADDRESS", 9999))
+    print("Connected successfully!")
+else:
+    print("Invalid choice")
