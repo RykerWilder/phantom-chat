0057f1f645d8030c1aa02cd542dc65a3858f63db
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-30 12:47:05 +0200

    fix: class P2P

--- main.py
+++ main.py
@@ -4,91 +4,7 @@ import socket
 import threading
 import sys
 
-def create_host(ip_addr, port, public_key):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        server.bind((ip_addr, port))
-        server.listen()
-        print(f"Server listening on {Fore.GREEN}{ip_addr}:{port}{Style.RESET_ALL}")
-        
-        client, client_address = server.accept()
-        
-        # Invio della chiave pubblica
-        client.send(public_key.save_pkcs1("PEM"))
-        
-        # Ricezione della chiave pubblica del partner
-        public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
-        
-        return client, public_partner
-    except Exception as e:
-        print(f"Error creating server {e}")
-        sys.exit(1)
-
-def create_connection(ip_addr, port, public_key):
-
-    print(f"Attempting to connect to {Fore.YELLOW}{ip_addr}:{port}{Style.RESET_ALL}...")
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        client.connect((ip_addr, port))
-        
-        # Invio della chiave pubblica
-        client.send(public_key.save_pkcs1("PEM"))
-        
-        public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
-        print("Keys exchange completed")
-        
-        return client, public_partner
-    except Exception as e:
-        print(f"{Fore.RED}Error while connecting {e}{Style.RESET_ALL}")
-        sys.exit(1)
-
-def sending_messages(client, public_partner):
-
-    try:
-        while True:
-            message = input("")
-
-            # EXIT
-            if message.lower() == "/exit":
-                print("Disconnection in progress...")
-                break
-                
-            # SEND ENCRYPTED MESSAGE
-            encrypted_message = rsa.encrypt(message.encode(), public_partner)
-            client.send(encrypted_message)
-            print(f"{Fore.YELLOW}You{Style.RESET_ALL}: {message}")
-    except Exception as e:
-        print(f"{Fore.RED}Error sending message: {e}{Style.RESET_ALL}")
-    finally:
-        # Close connection
-        client.close()
-        sys.exit(0)
-
-def receiving_messages(client, private_key):
-
-    try:
-        while True:
-            # RECEIVE AND DECRYPT MESSAGE
-            encrypted_message = client.recv(1024)
-            if not encrypted_message:
-                print(f"{Fore.RED}Partner disconnected.{Style.RESET_ALL}")
-                break
-                
-            message = rsa.decrypt(encrypted_message, private_key).decode()
-            print(f"{Fore.BLUE}Partner{Style.RESET_ALL}: {message}")
-    except Exception as e:
-        print(f"{Fore.RED}Error receiving message: {e}{Style.RESET_ALL}")
-    finally:
-        # CLOSE CONNECTION
-        client.close()
-        sys.exit(0)
-
 def main():
-
-    PORT = 9999
-    
-    # KEYS GENERATION
-    public_key, private_key = rsa.newkeys(1024)
 
     print("""
         [1] P2P
@@ -98,8 +14,12 @@ def main():
     choice = input(f"{Fore.GREEN}[?]Select your choice => {Style.RESET_ALL}")
 
     if choice == "1":
-        print("[INFO] You choose P2P. You can be a host or a client only in your network")
+        print(f"{Fore.BLUE}[INFO]{Style.RESET_ALL} You choose P2P. You can be a host or a client only in your network")
         start_P2P()
+    elif choice == "2":
+        print(f"{Fore.BLUE}[INFO]{Style.RESET_ALL} You choose public host.")
+    else: 
+        print(f"{Fore.RED}[X]Invalid choice{Style.RESET_ALL}")
     
 
 
--- phantom_chat/modules/P2P.py
+++ phantom_chat/modules/P2P.py
@@ -1,6 +1,16 @@
+from colorama import Style, Fore
+import rsa
+import socket
+import threading
+import sys
 
 class P2P:
+
+    # KEYS GENERATION
+    public_key, private_key = rsa.newkeys(1024)
+
     # IP ADDRESS
+    port = 9999
     default_ip = "192.168.1.38"
     ip_address = input(f"Insert the IP address (default: {default_ip}): ") or default_ip
 
@@ -12,11 +22,14 @@ class P2P:
 
     # START CONNECTION
     if choiceP2P == "1":
-        client, public_partner = create_host(ip_address, PORT, public_key)
+        client, public_partner = self.create_host(ip_address, port, public_key)
     else:
-        client, public_partner = create_connection(ip_address, PORT, public_key)
+        client, public_partner = self.create_connection(ip_address, port, public_key)
 
-    print(f"{Fore.GREEN}Connection established! Digit {Fore.YELLOW}\"/exit\"{Style.RESET_ALL}{Fore.GREEN} to exit.{Style.RESET_ALL}")
+    print(f"""
+        {Fore.GREEN}Connection established!{Style.RESET_ALL}
+        {Fore.YELLOW}Digit \"/exit\"{Style.RESET_ALL} to exit.
+    """)
 
     # SENDING AND RECEIVING
     send_thread = threading.Thread(target=sending_messages, args=(client, public_partner))
@@ -27,3 +40,83 @@ class P2P:
 
     send_thread.start()
     receive_thread.start()
+
+
+    def create_host(self, ip_addr, port, public_key):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((ip_addr, port))
+        server.listen()
+        print(f"Server listening on {Fore.GREEN}{ip_addr}:{port}{Style.RESET_ALL}")
+        
+        client, client_address = server.accept()
+        
+        # Invio della chiave pubblica
+        client.send(public_key.save_pkcs1("PEM"))
+        
+        # Ricezione della chiave pubblica del partner
+        public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+        
+        return client, public_partner
+    except Exception as e:
+        print(f"Error creating server {e}")
+        sys.exit(1)
+
+    def create_connection(self, ip_addr, port, public_key):
+
+        print(f"Attempting to connect to {Fore.YELLOW}{ip_addr}:{port}{Style.RESET_ALL}...")
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client.connect((ip_addr, port))
+            
+            # Invio della chiave pubblica
+            client.send(public_key.save_pkcs1("PEM"))
+            
+            public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+            print("Keys exchange completed")
+            
+            return client, public_partner
+        except Exception as e:
+            print(f"{Fore.RED}Error while connecting {e}{Style.RESET_ALL}")
+            sys.exit(1)
+
+    def sending_messages(self, client, public_partner):
+
+        try:
+            while True:
+                message = input("")
+
+                # EXIT
+                if message.lower() == "/exit":
+                    print("Disconnection in progress...")
+                    break
+                    
+                # SEND ENCRYPTED MESSAGE
+                encrypted_message = rsa.encrypt(message.encode(), public_partner)
+                client.send(encrypted_message)
+                print(f"{Fore.YELLOW}You{Style.RESET_ALL}: {message}")
+        except Exception as e:
+            print(f"{Fore.RED}Error sending message: {e}{Style.RESET_ALL}")
+        finally:
+            # Close connection
+            client.close()
+            sys.exit(0)
+
+    def receiving_messages(self, client, private_key):
+
+        try:
+            while True:
+                # RECEIVE AND DECRYPT MESSAGE
+                encrypted_message = client.recv(1024)
+                if not encrypted_message:
+                    print(f"{Fore.RED}Partner disconnected.{Style.RESET_ALL}")
+                    break
+                    
+                message = rsa.decrypt(encrypted_message, private_key).decode()
+                print(f"{Fore.BLUE}Partner{Style.RESET_ALL}: {message}")
+        except Exception as e:
+            print(f"{Fore.RED}Error receiving message: {e}{Style.RESET_ALL}")
+        finally:
+            # CLOSE CONNECTION
+            client.close()
+            sys.exit(0)
